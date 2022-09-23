bbb11118768ad4e6d339f6ebedb2f6c43a88d5f4
okhentai <kyakyadive@gmail.com>
2022-09-23 11:17:45 +0900

    Update

--- app/src/main/java/com/example/qr_code_row/Home.java
+++ app/src/main/java/com/example/qr_code_row/Home.java
@@ -4,11 +4,14 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
@@ -46,21 +49,29 @@ public class Home extends AppCompatActivity {
         options.setBeepEnabled(true);
         options.setOrientationLocked(true);
         options.setCaptureActivity(CaptureAct.class);
-        barLauncher.launch(options);
+        dh20lt.launch(options);
     }
-    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result ->
+    ActivityResultLauncher<ScanOptions> dh20lt = registerForActivityResult(new ScanContract(), result ->
     {
         if(result.getContents() != null)
         {
             AlertDialog.Builder builder = new AlertDialog.Builder(Home.this);
             builder.setTitle("Kết quả");
             builder.setMessage(result.getContents());
-            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            builder.setPositiveButton("Copy", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    ClipboardManager manager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                    ClipData data = ClipData.newPlainText("Ket qua:", result.getContents());
+                    manager.setPrimaryClip(data);
+                    Toast.makeText(Home.this, "Copied!", Toast.LENGTH_SHORT).show();
+                }
+            }).setNeutralButton("Ok", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     dialogInterface.dismiss();
                 }
-            }).show();
+            }).create().show();
         }
     });
 }
--- app/src/main/java/com/example/qr_code_row/Loading.java
+++ app/src/main/java/com/example/qr_code_row/Loading.java
@@ -7,18 +7,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class Loading extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_loading);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this,Home.class));
+                startActivity(new Intent(Loading.this,Home.class));
                 finish();
             }
         },2000);
